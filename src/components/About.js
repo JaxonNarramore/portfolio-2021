da62de7f2469e332b11337b6ed292270c983804d
Jaxon Narramore <jaxonnarramore@MacBook-Pro.attlocal.net>
2021-05-11 12:57:36 -0500

    color change

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
         <div id='about'>
             <div className='about-container'>
                 <div className="about-image">
-                    <img className='image' src="images/Me.jpeg" alt="" />
+                    <img className='image' src="images/Me.jpeg" alt="Jaxon Narramore" />
                 </div>
                 <div className='about-text'>
                     <p>I'm a full-stack software engineer located in Austin, Texas. My goal is to create engaging applications that add value to people's lives. I believe in a think-outside-the-box protocol, and incorporate it into my software with the use of creative problem solving, intuitive design, and keeping the customer experience in mind. I am constantly driven by my desire to be the best at what I do.</p>
--- src/components/ProjectItems.js
+++ src/components/ProjectItems.js
@@ -7,7 +7,7 @@ function ProjectItems(props) {
             <li className="cards__item">
                 <Link className="cards__item__link" to={{ pathname: props.path }} target='_blank'>
                     <figure className="cards__item__pic-wrap" data-category={props.label}>
-                        <img src={props.src} alt="Project Image" className="cards__item__img" />
+                        <img src={props.src} alt="Jaxon Narramore's Project Image" className="cards__item__img" />
                     </figure>
                     <div className="cards__item__info">
                         <h5 className="cards__item__text">{props.text}</h5>
